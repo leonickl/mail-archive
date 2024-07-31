9e0a49d3c0b59079d36db7e8a55c129353187c2a
Leo Nickl <leo@gehmasse.de>
2024-08-01 00:08:40 +0200

    remove inspire command

--- routes/console.php
+++ routes/console.php
@@ -1,13 +1,8 @@
 <?php
 
 use App\Models\Mail;
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Storage;
-
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
 
 Artisan::command('import', function () {
     if (Storage::directoryMissing('mails')) {
