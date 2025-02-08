16e4fee28a40ef32d40a965409ce5e919aff7f90
Leo <leo@gehmasse.de>
2025-02-09 00:06:28 +0100

    support mail viewing

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,11 +10,21 @@ class MailController extends Controller
     {
         return Mail::query()
             ->orderByDesc('date')
-            ->paginate(50);
+            ->paginate(20);
     }
 
     public function show(Mail $mail)
     {
         return $mail;
+    }
+
+    public function html(Mail $mail)
+    {
+        return $mail->body_html;
+    }
+
+    public function plain(Mail $mail)
+    {
+        return $mail->body_plain;
     }
 }
